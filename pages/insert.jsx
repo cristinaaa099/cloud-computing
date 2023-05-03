5e50d4bcffde8263d4d26b683bfa7e38533435b3
cristinaaa099 <cristina.marineata9@gmail.com>
2023-05-03 22:38:23 +0300

    fronted init

--- pages/insert.jsx
+++ pages/insert.jsx
@@ -1,12 +1,11 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-
-import MainPage from "@/js/components/MainPage";
+import InsertPage from "@/js/components/InsertPage";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-   <MainPage/>
+   <InsertPage/>
   )
 }
