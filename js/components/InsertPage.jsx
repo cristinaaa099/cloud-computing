635d7a0e4012373510d1d3020734778e44bb0f58
cristinaaa099 <cristina.marineata9@gmail.com>
2023-05-13 19:09:27 +0300

    final app

--- js/components/InsertPage.jsx
+++ js/components/InsertPage.jsx
@@ -17,32 +17,47 @@ export default function InsertPage() {
             document.getElementById("description").value = "";
         });
     }
+    const myFunc=(event)=>{
+        window.open("http://localhost:3000") ;
+    }
 
     return (
         <section className="bg-white dark:bg-gray-900">
             <div className="container px-6 py-10 mx-auto">
-                <h1 className="w-[500px] mx-auto text-center text-6xl">SuperCook App</h1>
-                <p className="w-[1000px] mx-auto text-center mt-4 text-3xl">Aceasta este o aplicatie cu retete culinare</p>
+                <button  style={{
+                    backgroundColor: '#48A860',
+                }} type="submit"
+                        onClick={ myFunc}
+                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Inapoi
+                </button>
+
+                <h1 style={{ color: '#0B6623' }} className="w-[500px] mx-auto text-center text-6xl">SuperCook App</h1>
+                <p style={{ color: '#679267' }} className="w-[1000px] mx-auto text-center mt-4 text-3xl">Aceasta este o aplicatie cu retete culinare</p>
 
                 <form>
                     <div className="mb-6">
                         <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Denumirea retetei</label>
-                        <input type="text" id="title"
+                        <input  type="text" id="title"
                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                placeholder="name@flowbite.com" required/>
                     </div>
                     <div className="mb-6">
                         <label htmlFor="description"
                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mod de preparare</label>
-                        <textarea id="description"
+                        <textarea  id="description"
                                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                   required/>
                     </div>
-                    <button type="submit"
+                    <button  style={{
+                        backgroundColor: '#48A860',
+                    }} type="submit"
                             onClick={ insertRecord }
                             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Adauga
                     </button>
                 </form>
+
+
+
             </div>
         </section>
     )
--- js/components/MainPage.jsx
+++ js/components/MainPage.jsx
@@ -33,16 +33,38 @@ export default function MainPage() {
             console.log(error);
         }
     }
-
+    const myFunc=(event)=>{
+        window.open("http://localhost:3000/insert") ;
+    }
+    const myFunc2=(event)=>{
+        window.open("http://localhost:3000/chat") ;
+    }
     return (
         <section className="bg-white dark:bg-gray-900">
             <div className="container px-6 py-10 mx-auto">
-                <h1 className="w-[500px] mx-auto text-center text-6xl">SuperCook App</h1>
-                <p className="w-[1000px] mx-auto text-center mt-4 text-3xl">Aceasta este o aplicatie cu retete culinare</p>
+                <button style={{
+                    backgroundColor: '#48A860',
+                }} type="submit"
+                        onClick={ myFunc}
+                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Adauga o reteta
+                </button>
+                <div style={{float: 'right'}}>
+                <button style={{
+                    backgroundColor: '#48A860',
+                }} type="submit"
+                        onClick={ myFunc2}
+                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Pune o intrebare
+                </button>
+                </div>
+                <h1 style={{ color: '#0B6623' }} className="w-[500px] mx-auto text-center text-6xl">SuperCook App</h1>
+                <p style={{ color: '#679267' }} className="w-[1000px] mx-auto text-center mt-4 text-3xl">Aceasta este o aplicatie cu retete culinare</p>
 
                 <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3">
                     {records.map(record => (
-                        <div
+                        <div  style={{
+                            backgroundColor: '#ACE1AF',
+                            borderRadius:  60,
+                        }}
                             key={record._id}
                             className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -52,7 +74,9 @@ export default function MainPage() {
                                 {record.description}
                             </p>
                             <div className={"flex justify-center mt-4"}>
-                                <button type="button"
+                                <button  style={{
+                                    backgroundColor: '#48A860',
+                                }} type="button"
                                         id={record._id}
                                         onClick={deleteRecord}
                                         className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Sterge
